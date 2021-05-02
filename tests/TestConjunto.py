4dc8572222d9740ad09d01a2f23817da407f1b0e
dgamarra <dgamarra@continental.edu.pe>
2021-05-02 09:07:30 -0500

    Caso 1: Calcular promedio con conjunto vacío (Verde)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -6,4 +6,3 @@ class TestConjunto ( unittest.TestCase ) :
     def test_conjunto_vacio_retornaNone ( self ) :
         conjunto = Conjunto ( [] )
         self.assertIsNone ( conjunto.promedio ( ) )
-
